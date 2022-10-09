dd7748a0090ff960f798c6aadf0368c37694b6f1
Lovish <lovishdakofficial@gmail.com>
2022-10-09 13:57:02 +0530

    name added in contributor.js ✔

--- contributors.js
+++ contributors.js
@@ -4,6 +4,11 @@ contributors = [
       fullname: "Shubhajeet Pradhan",
       username: "https://github.com/shubhajeet1207",
     },
+    {
+      id: 2,
+      fullname: "Lovish Dak",
+      username: "https://github.com/Lovish-Dak",
+    },
     // {
     //     id: sequence_number,
     //     fullname: "your_name",
